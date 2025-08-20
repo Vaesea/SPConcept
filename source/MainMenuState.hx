bb73740769d0044f06325bbc7aebf19d3c158fbf
Vaesea <159406785+Vaesea@users.noreply.github.com>
2025-08-20 11:27:29 +0100

    pepperTux

---
+++ source/MainMenuState.hx
@@ -0,0 +1,81 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.system.FlxAssets;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.ui.FlxButton;
+import flixel.util.FlxAxes;
+import flixel.util.FlxTimer;
+import openfl.system.System;
+
+class MainMenuState extends FlxState
+{
+    var playButton:FlxButton;
+    var closeButton:FlxButton;
+    var optionsButton:FlxButton;
+
+    override public function create()
+	{
+        super.create();
+
+        var logo = new FlxSprite();
+		logo.loadGraphic("assets/images/title/logo.png", false);
+        logo.x = 0;
+        logo.y = 25;
+		logo.screenCenter(FlxAxes.X);
+		add(logo);
+
+        FlxTween.tween(logo, {y: logo.y + 35}, 0.6, {ease: FlxEase.circInOut, type: PINGPONG});
+
+        // New button, center button, add button.
+        
+        playButton = new FlxButton(0, 250, "Play Game", clickPlay);
+        playButton.screenCenter(FlxAxes.X);
+        add(playButton);
+
+        optionsButton = new FlxButton(0, 300, "Options", clickOptions);
+        optionsButton.screenCenter(FlxAxes.X);
+        add(optionsButton);
+        
+        closeButton = new FlxButton(0, 350, "Close Game", clickClose);
+        closeButton.screenCenter(FlxAxes.X);
+        add(closeButton);
+
+        FlxG.sound.playMusic("assets/music/title.ogg", 1, true);
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+    
+    function clickPlay() // Switches to PlayState.
+    {
+        FlxG.sound.music.stop();
+        FlxG.sound.play("assets/music/titleEnd.ogg", 1.0);
+        remove(closeButton, true);
+        remove(optionsButton, true);
+        remove(playButton, true);
+        
+        new FlxTimer().start(8.0, completeTimer); // creates timer, starts it, 8.0 seconds, does completeTimer function below. lets gooooooo.
+    }
+
+    function completeTimer(timer:FlxTimer)
+    {
+        FlxG.switchState(new PlayState());
+    }
+
+    function clickOptions() // Switches to OptionsState. Simple.
+    {
+        FlxG.sound.music.stop();
+        FlxG.switchState(new OptionsState());
+    }
+
+    function clickClose() // Closes game.
+    {
+        System.exit(0);
+    }
+}
