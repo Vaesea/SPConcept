3fda26fcb29473e908f05bca1b9525e249fa88a4
Vaesea <159406785+Vaesea@users.noreply.github.com>
2025-08-20 16:55:29 +0100

    Create windows.bat
    [ci skip]

---
+++ setup/windows.bat
@@ -0,0 +1,15 @@
+@echo off
+color 0a
+cd ..
+@echo on
+echo Installing dependencies...
+echo This file is from FNF: Psych Engine
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.3
+haxelib install flixel 5.6.1
+haxelib install flixel-addons 3.2.2
+haxelib install flixel-tools 1.5.1
+haxelib set lime 8.1.2
+haxelib set openfl 9.3.3
+echo Finished!
+pause
